15938382d585f073b678cc8cf55136fc36ce7628
AIBusinessSupport <ai.deepmind.sci@gmail.com>
2023-08-09 22:09:52 +0900

    updated K-NN

--- demand/knn.py
+++ demand/knn.py
@@ -4,26 +4,19 @@ import sklearn
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.preprocessing import MinMaxScaler
 
-from ECVRP import TwoECVrp
+#from ECVRP import TwoECVrp
 
 #Get the Distance between Customers
-def KNN(path):
-    '''
-    path: os path of parameter data.
-    '''
-    # Get the whole info of Graph 
-    graph = TwoECVrp(path=path)
-    ## Get the useful info
-    n_customers = graph.n_customers
-    #cus_summary = np.array(graph.cus_sumary)
+def KNN(bound_demand, coord_cus):
+   
     
-    temp_demand = graph.demand         ## Get the demands of customers, the shape is (n_customers, 3)
+    temp_demand = bound_demand         ## Get the demands of customers, the shape is (n_customers, 3)
     mean_demand = temp_demand[:,1]
-    coord_customers = graph.upload_params_from_excel()['coor_custo']    ## Get the Coordinations of customers, the shape is (n_customers, 2)
+    coord_customers = coord_cus    ## Get the Coordinations of customers, the shape is (n_customers, 2)
     
     ## Get the prediction of demand of each customers. i-th value is the predicted demands of i-th customers
-    predict_demand = np.zeros(n_customers)
-    for i in range(n_customers):
+    predict_demand = np.zeros(len(bound_demand))
+    for i in range(len(bound_demand)):
         # Process Training data X and labels y
         X = coord_customers
         y = mean_demand - np.min(mean_demand)
@@ -38,7 +31,7 @@ def KNN(path):
         X = scaler.fit_transform(X)
         
         # Build the K-NN Regression Model and training
-        n_neighbors = int(np.sqrt(n_customers)) + 1
+        n_neighbors = int(np.sqrt(len(bound_demand))) + 1
         model = KNeighborsRegressor(n_neighbors=n_neighbors, weights='distance')
         model.fit(X, y)
         
